82a53b8032799a96b08e572116cee5e7ba74d8d4
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-11-11 16:47:24 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './UI/Home';
+import Menu from './features/menu/Menu';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+]);
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
