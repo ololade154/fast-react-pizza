03c30bf023914c7a0468aa7214b44ced25a71371
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-11-11 17:02:18 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './UI/Home';
 import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateUser from './features/user/CreateUser';
+import Order from './features/order/Order';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -10,6 +13,15 @@ const router = createBrowserRouter([
     path: '/menu',
     element: <Menu />,
   },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/user',
+    element: <CreateUser />,
+  },
+  { path: '/order', element: <Order /> },
 ]);
 const App = () => {
   return <RouterProvider router={router} />;
