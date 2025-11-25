2dd40f94c5f94608bca10a1b5632cee18ce90d29
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-11-25 08:38:27 +0100

    update

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 
 const CartOverview = () => {
   return (
-    <div className="bg-(--secondary)">
+    <div className="bg-(--secondary) sm:bg-(--primary) lg:bg-(--dark)">
       <p>
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      <Link to="/cart" className="font-semibold ">
+        Open cart &rarr;
+      </Link>
     </div>
   );
 };
